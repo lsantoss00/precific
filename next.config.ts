0a685d93982b1e44f2bd1d77827a0299493e8d46
fsvboas <fsvboas.dev@gmail.com>
2026-02-23 19:22:17 -0300

    chore: add next/bundle-analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,9 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
 import type { NextConfig } from "next";
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
 
 const nextConfig: NextConfig = {
   webpack(config, { dev }) {
@@ -61,4 +66,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
