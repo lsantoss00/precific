1ba8e0203d193060b7ed16f2165cd8ad02a44339
fsvboas <fsvboas.dev@gmail.com>
2026-03-04 13:50:13 -0300

    refactor: remove unnecessary try catch from use server services

--- src/app/(public)/criar-senha/services/new-password.ts
+++ src/app/(public)/criar-senha/services/new-password.ts
@@ -7,20 +7,16 @@ export async function newPassword({ password }: { password: string }) {
   const supabase = await createServer();
   const cookieStore = await cookies();
 
-  try {
-    const { error } = await supabase.auth.updateUser({ password });
-    if (error)
-      return {
-        success: false,
-        error: { code: error.code, message: error.message },
-      };
+  const { error } = await supabase.auth.updateUser({ password });
+  if (error)
+    return {
+      success: false,
+      error: { code: error.code, message: error.message },
+    };
 
-    cookieStore.set("recovery_mode", "", { expires: new Date(0), path: "/" });
-    cookieStore.set("invite_mode", "", { expires: new Date(0), path: "/" });
-    await supabase.auth.signOut();
+  cookieStore.set("recovery_mode", "", { expires: new Date(0), path: "/" });
+  cookieStore.set("invite_mode", "", { expires: new Date(0), path: "/" });
+  await supabase.auth.signOut();
 
-    return { success: true };
-  } catch (err) {
-    return { success: false, error: "Erro inesperado." };
-  }
+  return { success: true };
 }
